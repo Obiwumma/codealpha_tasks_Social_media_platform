c737ed2b9dcab18050037fdcb5870e2f705721e1
Obiwumma Pascal <pobiwumma@gmail.com>
2026-05-14 00:04:08 +0100

    Add user routes to main

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,4 +1,5 @@
 import  express from "express";
+import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,6 +9,8 @@ app.use(express.json())
 app.get("/api/health", (req, res) => {
   res.json({status: "ok", message: "Social media app is live"})
 });
+
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
