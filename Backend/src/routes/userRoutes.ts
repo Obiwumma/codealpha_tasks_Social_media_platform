63bfcf3ef211b92b22abeaf6a25a153059361b91
Obiwumma Pascal <pobiwumma@gmail.com>
2026-05-18 20:38:43 +0100

    Install bcrypt and hash password

--- Backend/src/routes/userRoutes.ts
+++ Backend/src/routes/userRoutes.ts
@@ -1,24 +1,31 @@
 import { Router } from "express";
 import { db } from "../db/index.js";
 import { users } from "../db/schema.js";
+import bcrypt from "bcrypt"
 
 const router = Router();
 
 router.post("/", async (req, res) => {
   try {
     // 1. We extract the data sent by the frontend
-    const { username, email } = req.body;
+    const { username, email, password } = req.body;
+    
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     const result = await db.insert(users).values({
       username,
-      email
+      email,
+      passwordHash: hashedPassword
     }).returning()
+
 
     const newUser = result[0]
 
     // 3. Respond to the frontend (Replace 'newUser' with whatever you name your result)
-    console.log('New User Created:', newUser); 
-    res.status(201).json(newUser);
+    const { passwordHash, ...safeUser } = newUser;
+
+    // 5. Respond with the safe version!
+    res.status(201).json(safeUser);
 
   } catch (error) {
     console.error(error);
